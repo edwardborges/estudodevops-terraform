a1df899868d834cbbf0e8aaafc1f1fd8dab18833
edwardborges <eduardoluizb@yahoo.com.br>
2026-02-16 17:58:44 +0000

    feat: add private subnet

---
+++ private.tf
@@ -0,0 +1,29 @@
+resource "aws_subnet" "eks_subnet_private_1a" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 3)
+  availability_zone = "${data.aws_region.current}a"
+
+
+  tags = merge(
+    local.tags,
+    {
+      Name                               = "comunidadedevops-priv-subnet-1a"
+      " kubernetes.io/role/internal-elb" = 1 # Necessario para elb  em uma sub-rede no eks
+    }
+  )
+}
+
+resource "aws_subnet" "eks_subnet_private_1b" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 4)
+  availability_zone = "${data.aws_region.current}b"
+
+
+  tags = merge(
+    local.tags,
+    {
+      Name                               = "comunidadedevops-priv-subnet-1b"
+      " kubernetes.io/role/internal-elb" = 1 # Necessario para elb  em uma sub-rede no eks
+    }
+  )
+}
