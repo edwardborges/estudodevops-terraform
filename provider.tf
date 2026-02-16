0a46a4b833448ed4eb8df95dd1c8511c0676594c
edwardborges <eduardoluizb@yahoo.com.br>
2026-02-16 15:46:13 +0000

    check

--- provider.tf
+++ provider.tf
@@ -1,9 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.28.0"
     }
+  }
+  backend "s3" {
+    bucket = "comunidade-devops"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+
   }
 }
 
