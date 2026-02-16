4e8f64a0f3af64ee46aeb54adb3910f7f6e2a64a
edwardborges <eduardoluizb@yahoo.com.br>
2026-02-16 19:07:03 +0000

    feat: add ngw e eipe para assocc private

--- private.tf
+++ private.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "eks_subnet_private_1a" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = cidrsubnet(var.cidr_block, 8, 3)
-  availability_zone = "${data.aws_region.current}a"
+  availability_zone = "${data.aws_region.current.name}a"
 
 
   tags = merge(
@@ -16,7 +16,7 @@ resource "aws_subnet" "eks_subnet_private_1a" {
 resource "aws_subnet" "eks_subnet_private_1b" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = cidrsubnet(var.cidr_block, 8, 4)
-  availability_zone = "${data.aws_region.current}b"
+  availability_zone = "${data.aws_region.current.name}b"
 
 
   tags = merge(
--- public.tf
+++ public.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "eks_subnet_public_1a" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
-  availability_zone       = "${data.aws_region.current}a"
+  availability_zone       = "${data.aws_region.current.name}a"
   map_public_ip_on_launch = true
 
 
@@ -17,7 +17,7 @@ resource "aws_subnet" "eks_subnet_public_1a" {
 resource "aws_subnet" "eks_subnet_public_1b" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
-  availability_zone       = "${data.aws_region.current}b"
+  availability_zone       = "${data.aws_region.current.name}b"
   map_public_ip_on_launch = true
 
   tags = merge(
@@ -28,3 +28,13 @@ resource "aws_subnet" "eks_subnet_public_1b" {
     }
   )
 }
+
+resource "aws_route_table_association" "eks_rtb_assoc_1a" { # associacao da route_table publica
+  subnet_id      = aws_subnet.eks_subnet_public_1a.id
+  route_table_id = aws_route_table.eks_public_route_table.id
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_1b" { # associacao da route_table publica
+  subnet_id      = aws_subnet.eks_subnet_public_1b.id
+  route_table_id = aws_route_table.eks_public_route_table.id
+} 
