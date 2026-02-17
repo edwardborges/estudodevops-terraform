a7e9e47e7a44a43a0a89e7ebfeb2d7b8e0415647
edwardborges <eduardoluizb@yahoo.com.br>
2026-02-17 17:27:26 +0000

    feat: add iam policy

--- modules/cluster/iam.tf
+++ modules/cluster/iam.tf
@@ -22,3 +22,10 @@ resource "aws_iam_role" "eks_cluster_role" {
     }
   )
 }
+
+resource "aws_iam_policy_attachment" "eks_cluster_role_attachment" {
+  name       = "${var.project_name}-cluster-policy-attachment"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+
+  roles = [aws_iam_role.eks_cluster_role.name] # pesquisando chatgpt ele deu essa informacao sempre exige um destes campos role,users,groups
+}
