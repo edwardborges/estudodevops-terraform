ef9efac2dcf3e2f27266ca8543117c27d3703983
edwardborges <eduardoluizb@yahoo.com.br>
2026-02-18 15:19:18 +0000

    feat: certificate e oidc

--- modules/cluster/oidc.tf
+++ modules/cluster/oidc.tf
@@ -1,3 +1,12 @@
 data "tls_certificate" "eks_oidc_tls_certificate" {
   url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
 }
+#pega o certificado TLS do endpoint OIDC do cluster
+#extrai o thumbprint
+#que depois será usado para criar o provider IAM
+
+resource "aws_iam_openid_connect_provider" "eks_oidc" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = data.tls_certificate.eks_oidc_tls_certificate.certificates[*].sha1_fingerprint
+  url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+} 
--- modules/cluster/output.tf
+++ modules/cluster/output.tf
@@ -3,5 +3,6 @@ output "eks_vpc_config" {
 }
 
 output "oidc" {
-  value = aws_eks_cluster.eks_cluster.identity
+  value = data.tls_certificate.eks_oidc_tls_certificate.certificates[*].sha1_fingerprint
 }
+
