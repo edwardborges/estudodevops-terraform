bf48df51a01ac98fbf1163b9d1b6fe03e09f3b57
edwardborges <eduardoluizb@yahoo.com.br>
2026-02-18 18:05:53 +0000

    feat: criacao do modules novos

---
+++ modules/aws-load-balancer-controller/policy.tf
@@ -0,0 +1,7 @@
+resource "aws_iam_policy" "eks_controller_policy" {
+  name = "${var.project_name}-awsload-balancer-controller"
+
+  policy = file("${path.module}/iam_policy.json")
+
+  tags = var.tags
+}
